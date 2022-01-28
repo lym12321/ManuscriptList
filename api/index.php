0035e20dc9e2e70e11d3c79a1450afa47adf9174
lym12321 <41183806+lym12321@users.noreply.github.com>
2022-01-28 20:09:29 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -8,13 +8,49 @@
         $s = file_get_contents($url);
         return $s;
     }
+    function cut_str($str,$len,$suffix=" ... ... "){
+        $strlen = strlen($str);
+        if($strlen > $len){
+            if(function_exists('mb_substr')){
+                $str = mb_substr($str,0,$len/2,'utf-8').$suffix.mb_substr($str,-$len/2,$len/2,'utf-8');
+                // die($str);
+            }else{
+                report(-1, "未支持 mb_substr 函数", array());
+            }     
+        } 
+        return $str; 
+    }
     
     $method = $_GET['method'];
     if(!$method){
-        report(404, "缺少参数", array());
+        report(-1, "缺少参数", array());
+    }
+
+    if($method == "getNotice"){
+        if(!$notice) report(404, "不存在公告", array());
+        report(200, "success", array('notice' => $notice));
+    }
+    
+    if($method == "getDetail"){
+        // 跳转到答卷详情页面（由问卷网提供）
+        $seq = $_GET['seq'];
+        if(!$seq) report(-1, "缺少参数", array());
+        if(!is_numeric($seq)) report(-1, "参数不合法", array());
+        
+        $url = "https://open.wenjuan.com/openapi/v4/get_rspd_url";
+        
+        $timestamp = time();
+        $signature = md5($AppKey.$seq.$formId.$timestamp.$AppSecret);
+        $tmp = get($url."?app_key=".$AppKey."&timestamp=".$timestamp."&seq=".$seq."&short_id=".$formId."&signature=".$signature);
+        
+        if($_GET['debug'] == "true") die($tmp);
+        
+        $json = json_decode($tmp, true);
+        header('location:'.$json["data"]["url"]);
     }
     
     if($method == "getList"){
+        $url = "https://open.wenjuan.com/openapi/v4/get_rspd_list";
         // Q1 - 匿名/实名
         // Q2 - 真实姓名
         // Q3 - 昵称
@@ -30,8 +66,12 @@
         $timestamp = time();
         $signature = md5($AppKey.$page.$formId.$timestamp.$AppSecret);
         
-        $json = json_decode(get("https://open.wenjuan.com/openapi/v4/get_rspd_list?app_key=".$AppKey."&timestamp=".$timestamp."&page=".$page."&short_id=".$formId."&signature=".$signature), true);
-        if($_GET['debug']) print_r($json);
+        $tmp = get($url."?app_key=".$AppKey."&timestamp=".$timestamp."&page=".$page."&short_id=".$formId."&signature=".$signature);
+        
+        if($_GET['debug'] == "true") die($tmp);
+        
+        $json = json_decode($tmp, true);
+        // if($_GET['debug']) print_r($json);
         
         $res = array();
         
@@ -55,11 +95,11 @@
             if($list[$i]["Q7"]["answer"]) $video = explode("&&", $list[$i]["Q7"]["answer"]);
             else $video = [];
             array_push($res, array(
-                'id' => $i + ($page - 1) * $json["data"]["page_size"] + 1,
+                'id' => $i + ($page - 1) * $json["data"]["page_size"] + 1, // 这是正序的 id ，注意！
                 'name' => $name, 
                 'time' => $list[$i]["finish"], 
                 'ip' => $list[$i]["ip"], 
-                'text' => $list[$i]["Q5"]["answer"],
+                'text' => cut_str($list[$i]["Q5"]["answer"], 200),
                 'image' => array('name' => $image[0], 'url' => $image[1]),
                 'video' => array('name' => $video[0], 'url' => $video[1])
             ));
